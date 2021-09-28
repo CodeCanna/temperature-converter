41d17cb04fcdb735e21fbe824d7e80cb1dba5746
CodeCanna <codecannamw@gmail.com>
2021-09-28 15:11:44 -0600

    Added tolerance values for my tests.

--- src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
@@ -5,17 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TemperatureConverterTest {
-
+  static final double FAHRENHEIT_TOLERANCE = 0.001;
+  static final double CELSIUS_TOLERANCE = 0.0005;
   static final double[] testValueF = {-40, 32, 50};
   static final double[] testValueC = {-40, 0, 10};
 
   @Test
   void convertC2F() {
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < testValueC.length; i++) {
       double f = TemperatureConverter.convertC2F(testValueC[i]);
       double tf = testValueF[i];
 
-      assertEquals(tf, f);
+      assertEquals(tf, f, FAHRENHEIT_TOLERANCE);
     }
   }
 }
